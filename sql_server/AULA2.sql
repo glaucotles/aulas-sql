a44d05a9a17ba3bc11cef98e20ac1b29da97c89c
Glauco Teles <155410070+glaucotles@users.noreply.github.com>
2026-02-13 08:50:24 -0300

    Add files via upload

---
+++ sql_server/AULA2.sql
@@ -0,0 +1,15 @@
+-- Selecionar tudo da tabela DimStore
+SELECT 
+	*
+FROM 
+	DimStore
+
+-- Selecionar determinadas colunas da tabela DimStore (ID, Nome e Numero de Telefone)
+/*SELECT
+	StoreKey,
+	StoreName,
+	StorePhone
+FROM
+	DimStore*/
+
+SELECT * FROM DimProduct
